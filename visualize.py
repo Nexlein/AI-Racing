50237e3c6db4bc21ddb14a06767ac746fa451ddd
Nielsen Combe-Bracciale <nielsen.combe-bracciale@epitech.eu>
2026-08-21 19:29:50 +0200

    Refactor event handling in main loop for clarity

--- visualize.py
+++ visualize.py
@@ -170,10 +170,8 @@ def main():
 
     while running:
         for event in pygame.event.get():
-            if (
-                event.type == pygame.QUIT
-                or event.type == pygame.KEYDOWN
-                and event.key == pygame.K_ESCAPE
+            if event.type == pygame.QUIT or (
+                event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
             ):
                 running = False
 
